c5594b5d43fbd8ce5ab72ac40bd0e417269f211c
mnaoi <hyazinthe@gmail.com>
2020-06-20 04:01:04 +0900

    added user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/show'
+    get 'users/index'
+  end
   # get 'tasks/index'
   # get 'tasks/show'
   # get 'tasks/new'
